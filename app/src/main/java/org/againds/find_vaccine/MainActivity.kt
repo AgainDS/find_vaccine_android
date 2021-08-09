fee4bbac07e723cef431c5f2ee2cf6433f8d3395
daeni-dang <de021@naver.com>
2021-08-09 16:57:36 +0900

    2

--- app/src/main/java/org/againds/find_vaccine/MainActivity.kt
+++ app/src/main/java/org/againds/find_vaccine/MainActivity.kt
@@ -1,11 +1,22 @@
 package org.againds.find_vaccine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import net.daum.mf.map.api.MapView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView.setImageResource(R.drawable.start)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivityForResult(intent, 101)
+        }
+//        val map = MapView(this)
+//        mapView.addView(map)
     }
 }
