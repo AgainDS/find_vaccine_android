11945d48759da3fca2cc8e0542641ff0d25541af
daeni-dang <de021@naver.com>
2021-08-10 15:25:20 +0900

    3

--- app/src/main/java/org/againds/find_vaccine/LoginActivity.kt
+++ app/src/main/java/org/againds/find_vaccine/LoginActivity.kt
@@ -2,10 +2,16 @@ package org.againds.find_vaccine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "로그인 되었습니다.", Toast.LENGTH_LONG).show()
+        }
     }
 }
